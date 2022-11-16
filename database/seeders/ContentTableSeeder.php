3dacbb0220aa97f957f3734e13368427c8f78df5
Maarten Wolfsen <maartenwolfsen@gmail.com>
2022-11-16 09:12:05 +0100

    Add migration & seeder

---
+++ database/seeders/ContentTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $names = ['Jos', 'Thomas', 'Maarten', 'Arjan'];
+
+            DB::table('content')->insert([
+                'title' => 'Wie is hier nou de hosting!?',
+                'image' => 'https://media.tenor.com/jf2rMCZMYv0AAAAC/snackbar-new-kids.gif',
+                'value' => $names[array_rand($names)]  . ' approves this message.'
+            ]);
+        }
+    }
+}
